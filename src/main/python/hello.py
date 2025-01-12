4371e0230739dd4d11ac310c2a7c8ad4b9504f19
rost5000 <rost.26@mail.ru>
2025-01-12 22:08:55 +0100

    Secod commit

--- src/main/python/hello.py
+++ src/main/python/hello.py
@@ -1,4 +1,22 @@
-from termcolor import colored
+import datetime
 
-colored_text = colored("hello java", "red", attrs=["reverse", "blink"])
-print(colored_text)
+import os
+from git import Repo
+
+base_file = "../resources/dummy_file.txt"
+base_file_path = os.path.abspath(base_file)
+
+
+repo = Repo('../../../')
+start_date = datetime.datetime.now(datetime.UTC)
+
+end_date = datetime.datetime.now(datetime.UTC) - datetime.timedelta(days=5)
+delta = datetime.timedelta(days=1)
+index = repo.index
+
+while start_date >= end_date:
+    with open(base_file_path, "wt") as f:
+        f.write(start_date.__str__())
+    index.add([base_file_path])
+    index.commit("A commit message", commit_date=start_date, author_date=start_date)
+    start_date -= delta
